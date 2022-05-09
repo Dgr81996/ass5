e72f1d8278732f2e0058e0c386e0e44b3d276424
Deepak Choudhary <97680719+Dgr81996@users.noreply.github.com>
2022-05-09 17:17:16 +0530

    Add files via upload

---
+++ 8a/src/App.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+function App() {
+  return (
+    <div>
+    
+
+<div style={container}>
+
+
+
+<div>
+<img source={{uri:"https://i.imgur.com/lMeVwr7.png"}} />
+<p>continent pizza,asian dessert</p>
+<p> min $50</p>
+<p>accepts only payment  online </p>
+
+</div>
+
+
+
+
+
+
+</div>
+    </div>
+  );
+}
+
+export default App;
